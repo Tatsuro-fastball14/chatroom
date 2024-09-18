25f0d9cff62fe77fa1c01ca7c310d64c719474d7
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2024-09-18 12:16:21 +0900

    Implementing chat room creation functionality
    Implementing chat room creation functionality

--- frontend/src/components/ChatRooms.vue
+++ frontend/src/components/ChatRooms.vue
@@ -6,19 +6,25 @@
         <router-link :to="`/rooms/${room.id}`">{{ room.name }}</router-link>
       </li>
     </ul>
+    <h3>チャットルーム作成</h3>
+    <input type="text" v-model="newRoomName" />
+    <div>
+      <button @click="createRoom">作成</button>
+    </div>
   </div>
 </template>
-
 <script>
 import axios from 'axios'
 
 export default {
   data() {
     return {
-      chatRooms: []
+      chatRooms: [],
+      newRoomName: ''
     }
   },
-  mounted() {
+  created() {
+    // コンポーネントが作成されたときに、チャットルーム一覧を取得する
     this.fetchChatRooms()
   },
   methods: {
@@ -27,6 +33,19 @@ export default {
         .get('http://localhost:3000/rooms')
         .then((response) => {
           this.chatRooms = response.data
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    },
+    createRoom() {
+      axios
+        .post('http://localhost:3000/rooms', {
+          name: this.newRoomName
+        })
+        .then((response) => {
+          this.chatRooms.push(response.data)
+          this.newRoomName = ''
         })
         .catch((error) => {
           console.error(error)
